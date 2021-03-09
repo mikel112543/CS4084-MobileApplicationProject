0f5b1c321f396a7b48d59acb1cddaf905fa0054d
banditshabs <48322890+banditshabs@users.noreply.github.com>
2021-03-09 20:25:54 +0000

    Added step 1 of creating an account: Asking phone for permission to have access to gallery to set up a profile picture.

---
+++ app/src/main/java/com/example/cs4084_project_farm_market/ProfileSetUp.java
@@ -0,0 +1,94 @@
+package com.example.cs4084_project_farm_market;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class ProfileSetUp extends AppCompatActivity {
+
+    //VIEWS
+    ImageView userProfilePic ;
+    Button chooseUserProfilePicButton ;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_set_up);
+
+        //VIEWS
+        chooseUserProfilePicButton = findViewById(R.id.chooseProfilePicButton);
+        userProfilePic = findViewById(R.id.profilePicture);
+        //Create a toolbar (Title on top of the page showing where you are)
+        Toolbar toolbar = findViewById(R.id.toolbarProfileSetUp);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Profile Set Up");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        //handle button click
+        chooseUserProfilePicButton.setOnClickListener(v -> {
+            //check runtime permission
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                //permission not granted, request it.
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                //show popup for runtime permission
+                requestPermissions(permissions, PERMISSION_CODE);
+            }
+            else {
+                //Permission already granted
+                pickImageFromGallery();
+            }
+        });
+
+    }
+
+    private void pickImageFromGallery() {
+        //intent to pick image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_CODE);
+    }
+
+    //handle result of runtime permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //permission was granted
+                pickImageFromGallery();
+            } else {
+                //permission was denied
+                Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            //set Image to imageView
+            userProfilePic.setImageURI(data.getData());
+        }
+    }
+
+
+}
