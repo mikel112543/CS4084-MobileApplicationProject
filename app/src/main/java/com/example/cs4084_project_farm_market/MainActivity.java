6399daf3848628b13fc2fd59b7ae4e66e78908e0
Mikel112543 <michael.danaher112543@gmail.com>
2021-03-23 18:06:07 +0000

    - Added Toolbar with Profile Icon and Labels
    Signed-off-by: Mikel112543 <michael.danaher112543@gmail.com>

--- app/src/main/java/com/example/cs4084_project_farm_market/MainActivity.java
+++ app/src/main/java/com/example/cs4084_project_farm_market/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,29 +24,46 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+        Toolbar toolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
+        getSupportActionBar().setTitle("Home");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.home_toolbar_menu, menu);
+        return true;
     }
 
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Toolbar toolbar = findViewById(R.id.home_toolbar);
+                    setSupportActionBar(toolbar);
                     Fragment selectedFragment = null;
 
                     switch (item.getItemId()) {
                         case R.id.nav_home:
                             selectedFragment = new HomeFragment();
+                            toolbar.setTitle("Home");
                             break;
                         case R.id.nav_search:
                             selectedFragment = new SearchFragment();
+                            toolbar.setTitle("Search");
                             break;
                         case R.id.nav_notification:
                             selectedFragment = new NotificationFragment();
+                            toolbar.setTitle("Notifications");
                             break;
                         case R.id.nav_messages:
                             selectedFragment = new MessagesFragment();
+                            toolbar.setTitle("Messages");
                             break;
 
                     }
