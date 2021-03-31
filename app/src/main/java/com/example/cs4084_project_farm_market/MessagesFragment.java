15a7216f3d6d15ed237e2efaa8235fe2595a2fa7
Michael Danaher <mikel.d@live.ie>
2021-03-31 22:58:17 +0100

    - Added Animations and Scroll view to Fragments - Added fully working toolbar to Fragments
    Signed-off-by: Mikel112543 <michael.danaher112543@gmail.com>

--- app/src/main/java/com/example/cs4084_project_farm_market/MessagesFragment.java
+++ app/src/main/java/com/example/cs4084_project_farm_market/MessagesFragment.java
@@ -2,6 +2,7 @@ package com.example.cs4084_project_farm_market;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -58,7 +59,12 @@ public class MessagesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View mView = inflater.inflate(R.layout.fragment_messages, container, false);
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_messages, container, false);
+
     }
 }
