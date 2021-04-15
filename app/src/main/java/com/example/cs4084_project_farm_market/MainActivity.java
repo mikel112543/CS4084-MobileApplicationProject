986e379861cc4364919e8c9f3074b65a654f24f8
Aaron <18232086@studentmail.ul.ie>
2021-04-15 15:41:40 +0100

    Settings Activity, have the code in main activity commented out think this will go in Bandis profile one as in the button is made from there.

--- app/src/main/java/com/example/cs4084_project_farm_market/MainActivity.java
+++ app/src/main/java/com/example/cs4084_project_farm_market/MainActivity.java
@@ -73,3 +73,36 @@ public class MainActivity extends AppCompatActivity {
             };
 }
 
+/*
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b_settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b_settings = (Button) findViewById(R.id.b_settings);
+
+        b_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
+
+
+ */
+
