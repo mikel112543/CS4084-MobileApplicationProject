02df4bc108f5b7fc66891bf2e40784b600224fc0
Mikel112543 <michael.danaher112543@gmail.com>
2021-04-15 23:21:32 +0100

    - Cleaned all activities linked to this branch
    Signed-off-by: Mikel112543 <michael.danaher112543@gmail.com>

--- app/src/main/java/com/example/cs4084_project_farm_market/LoginActivity.java
+++ app/src/main/java/com/example/cs4084_project_farm_market/LoginActivity.java
@@ -1,20 +1,18 @@
 package com.example.cs4084_project_farm_market;
 
-import androidx.annotation.DrawableRes;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
+
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,7 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private Button loginButton;
     private Button registerButton;
-    private TextView  txt_passReset;
+    private TextView txt_passReset;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -43,7 +41,6 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(new Intent(LoginActivity.this, MainActivity.class));  //Load Main activity if user is logged in
             finish();
         }*/
-
         setContentView(R.layout.activity_login);
 
         txt_email = findViewById(R.id.txt_email);
@@ -51,50 +48,62 @@ public class LoginActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.btn_register);
         txt_passReset = findViewById(R.id.txt_passReset);
         loginButton = findViewById(R.id.btn_login);
-
-        Drawable buttonInline = getResources().getDrawable(R.drawable.button);
-
-        loginButton.setBackground(buttonInline);
-        registerButton.setPadding(10,10,10,10);
+        loginButton.setOnClickListener(loginButtonListener);
+        registerButton.setOnClickListener(registerButtonListener);
+        txt_passReset.setOnClickListener(passResetListener);
 
         auth = FirebaseAuth.getInstance();
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                confirmLogin(v);
-            }
-        });
-
-        txt_passReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, PasswordResetActivity.class));
-            }
-        });
     }
 
+    /**
+     * OnClick for login button
+     */
+    private final View.OnClickListener loginButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            confirmLogin(v);
+        }
+    };
 
-    public void confirmLogin(View view) {
+    /**
+     * OnClick for register button
+     */
+    private final View.OnClickListener registerButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        }
+    };
+
+    /**
+     * OnClick for password reset
+     */
+    private final View.OnClickListener passResetListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(LoginActivity.this, PasswordResetActivity.class));
+        }
+    };
+
+
+    /**
+     * Validate users login credentials and log them in.
+     *
+     * @param view
+     */
+    private void confirmLogin(View view) {
         boolean validation = validateEmail() && validatePassword();     //Both methods must return true
 
-        if(validation) {
+        if (validation) {
             String email = txt_email.getEditText().getText().toString();
             String password = txt_password.getEditText().getText().toString();
 
             auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                 @Override   //Calling Firebase login method
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(!task.isSuccessful()) {
+                    if (!task.isSuccessful()) {
                         Toast.makeText(LoginActivity.this, ("Login Failed!"), Toast.LENGTH_LONG).show();
-                    }else{
+                    } else {
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(intent);
                         finish();
@@ -104,30 +113,40 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public boolean validateEmail() {
+    /**
+     * Validate email
+     *
+     * @return true if validation ok. False otherwise
+     */
+    private boolean validateEmail() {
         String email = txt_email.getEditText().getText().toString();
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
-        if(email.isEmpty()) {
+        if (email.isEmpty()) {
             txt_email.setError("Field can't be empty");
             return false;
-        }else if(!email.matches(emailPattern)) {
+        } else if (!email.matches(emailPattern)) {
             txt_email.setError("Please enter a valid email");
             return false;
-        }else {
+        } else {
             txt_email.setError(null);
             return true;
         }
 
     }
 
+    /**
+     * Validate Password
+     *
+     * @return true is validation ok. False otherwise.
+     */
     private boolean validatePassword() {
         String password = txt_password.getEditText().getText().toString();
 
-        if(password.isEmpty()) {
+        if (password.isEmpty()) {
             txt_password.setError("Field can't be empty");
             return false;
-        }else {
+        } else {
             txt_password.setError(null);
             return true;
         }
